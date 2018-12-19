c82d44bfebc10b67285cf038be8bb8d668ecbbfc
isas1 <teachmecomputerscience@gmail.com>
2018-12-19 17:58:21 +0000

    hot-loader added to retain state when data is changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { hot } from 'react-hot-loader'
 
 class App extends React.Component {
 
@@ -9,13 +10,15 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello World!</h1>
-        <h2>Count: {this.state.count} </h2>
-        <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
-        <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
+        <h1>Hola World!!</h1>
+        <h2 className={this.state.count > 10 ? 'warning' : 'lower'}>
+          Count: {this.state.count}
+        </h2>
+        <button onClick={() => this.setState(state => ({ count: state.count + 1 }))}>+</button>
+        <button onClick={() => this.setState(state => ({ count: state.count - 1 }))}>-</button>
       </div>
     )
   }
 }
 
-export default App
+export default hot(module)(App)
