090f46d4c956c517213ee0744a9062fffa91e1b9
isas1 <teachmecomputerscience@gmail.com>
2018-12-19 15:18:38 +0000

    babel loader and support for react added

--- src/greet.js
+++
@@ -1,3 +0,0 @@
-const greeting = "Hello world!"
-
-export default greeting
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import greeting from './greet'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-console.log(greeting)
+ReactDOM.render(<App/>, document.getElementById('app'))
